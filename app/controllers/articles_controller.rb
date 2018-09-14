bcf62da0665c5bc8546a7f1cb97095dd104e3489
boomerang95 <kinkstill@bk.ru>
2018-09-14 13:36:10 +0300

    5.8. Отображение всех статей

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+	
+	def index
+    @articles = Article.all
+  end
 
 	def show
     @article = Article.find(params[:id])
@@ -20,3 +24,4 @@ private
 
 
 end
+
